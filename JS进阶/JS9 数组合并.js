2075a6ca571b48feb86f5f1cce21351fce39c1bc
Angelia-cj <cjia0620@gmail.com>
2021-12-09 17:02:00 +0800

    JS9 数组合并

--- JS进阶/JS9 数组合并.js
+++ JS进阶/JS9 数组合并.js
@@ -3,7 +3,7 @@
  * @Author: changjia
  * @Date: 2021-12-09 16:58:52
  * @LastEditors: changjia
- * @LastEditTime: 2021-12-09 16:58:52
+ * @LastEditTime: 2021-12-09 17:01:06
  */
 function concat(arr1, arr2) {
   let newArr = [...arr1, ...arr2]
@@ -14,3 +14,9 @@ let arr1 = [1, 2, 3, 4],
 
 let newArr = [...arr1, ...arr2]
 console.log(newArr);
+
+let newArr1 = concat(arr1, arr2)
+console.log(newArr1);
+
+let newArr2 = arr1.concat(arr2)
+console.log(newArr2);
