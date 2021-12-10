bd92cebcc3436a922a81bbba0e7462ef02d2adce
Angelia-cj <cjia0620@gmail.com>
2021-12-10 11:32:43 +0800

    FED39 数组最大值

---
+++ 零基础入门前端/FED39 数组最大值.js
@@ -0,0 +1,12 @@
+/*
+ * @description: 
+ * @Author: changjia
+ * @Date: 2021-12-10 11:30:51
+ * @LastEditors: changjia
+ * @LastEditTime: 2021-12-10 11:32:18
+ */
+let arr = [1, 4, 2, 6, 4, 37]
+console.log(...arr);
+
+let max = Math.max(...arr)
+console.log(max);
