e7e0a70125949a9842a44d978322cd2b68b39c3a
Angelia-cj <cjia0620@gmail.com>
2021-12-10 11:27:51 +0800

    ED38 数组转字符串FED38 数组转字符串

---
+++ 零基础入门前端/FED38 数组转字符串.js
@@ -0,0 +1,10 @@
+/*
+ * @description: 
+ * @Author: changjia
+ * @Date: 2021-12-10 11:25:16
+ * @LastEditors: changjia
+ * @LastEditTime: 2021-12-10 11:26:29
+ */
+let arr = [1, '2', 3]
+let str = arr.join('')
+console.log(str);
